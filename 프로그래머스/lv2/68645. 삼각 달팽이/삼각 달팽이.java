5de2652137f5a4e811dfbb9166a6a6e9b912f8fd
jo.belief <jmd5314@gmail.com>
2023-09-28 15:10:31 +0900

    [level 2] Title: 삼각 달팽이, Time: 24.53 ms, Memory: 137 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,33 @@
+class Solution {
+    private static final int dx[] = {0,1,-1};
+    private static final int dy[] = {1,0,-1};
+    public int[] solution(int n) {
+        int triangle [][] = new int [n][n];
+        int v = 1;
+        int x = 0;
+        int y = 0;
+        int d = 0;
+        while(true){
+            triangle[y][x] = v++;
+            int nx = x+dx[d];
+            int ny = y+dy[d];
+            if(nx==-1||ny==-1||nx==n||ny==n||triangle[ny][nx]!=0){
+                d=(d+1)%3;
+                nx = x+dx[d];
+                ny = y+dy[d];
+                if(nx==-1||ny==-1||nx==n||ny==n||triangle[ny][nx]!=0)
+                    break;
+            }
+            x=nx;
+            y=ny;
+        }
+        int index = 0;
+        int result[] = new int [v-1];
+        for(int i =0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                result[index++] = triangle[i][j];
+            }
+        }
+        return result;
+        }
+    }
