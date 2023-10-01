8ee3ed6563b55fa8bee1cc7d0b96fda400c94311
jo.belief <jmd5314@gmail.com>
2023-10-02 00:00:11 +0900

    [Bronze II] Title: 커트라인, Time: 260 ms, Memory: 18996 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+        int grade[] = new int[N];
+        for(int i=0;i<N;i++){
+            grade[i] = sc.nextInt();
+        }
+        Arrays.sort(grade);
+        System.out.println(grade[N-k]);
+    }
+}
