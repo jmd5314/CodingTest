68dd74b4dc4607d9ff0efafea0e9640d7454f0f3
jo.belief <jmd5314@gmail.com>
2024-01-26 23:53:32 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 224 ms, Memory: 15672 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String S  = st.nextToken();
+        st= new StringTokenizer(br.readLine());
+        int q = Integer.parseInt(st.nextToken());
+        int length = S.length();
+        int count[][] = new int[length][26];
+        char ch [] = S.toCharArray();
+        count[0][ch[0]-'a'] = 1;
+        for(int i =1;i<length;i++){
+            for(int j=0;j<26;j++){
+                if(j==ch[i]-'a')
+                    count[i][ch[i]-'a'] = count[i-1][ch[i]-'a']+1;
+                else
+                    count[i][j] = count[i-1][j];
+            }
+        }
+        for(int i = 0;i<q;i++){
+            st = new StringTokenizer(br.readLine());
+            char a = st.nextToken().charAt(0);
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            if(l==0)
+                System.out.println(count[r][a-'a']);
+            else
+                System.out.println(count[r][a-'a'] - count[l-1][a-'a']);
+            
+        }
+    }
+}
