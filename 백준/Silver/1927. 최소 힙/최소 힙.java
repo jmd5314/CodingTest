1f5ef2f9b80366884b434041e34d6eccb0c0869b
jo.belief <jmd5314@gmail.com>
2024-03-14 00:01:39 +0900

    [Silver II] Title: 최소 힙, Time: 332 ms, Memory: 26804 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            //x가 0일때 비어있으면 0을 출력, 비어있지 않으면 맨 앞에 위치한 값을 제거, 출력
+            if(x == 0) {
+                if(pq.isEmpty()) sb.append(0).append("\n");
+                else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            }else {
+                pq.offer(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
