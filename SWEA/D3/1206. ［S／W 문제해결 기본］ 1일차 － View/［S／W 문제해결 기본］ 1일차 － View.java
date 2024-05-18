f559d870f53c5e91b5d6f98c15770b0b8f238f04
jo.belief <jmd5314@gmail.com>
2024-05-19 00:52:12 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 188 ms, Memory: 40,156 KB -BaekjoonHub

---
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int T = 10;
+        int index = 1;
+        		while(T-->0) {
+			int sum = 0;
+			int N = sc.nextInt();
+			int [] height = new int[N];
+			for(int i =0;i<N;i++) {
+				height[i] = sc.nextInt();
+			}
+			for(int i =2;i<N-2;i++) {
+				int leftMax = Math.max(height[i-1], height[i-2]);
+				int rightMax = Math.max(height[i+1], height[i+2]);
+				int count = Math.min(height[i]-leftMax, height[i]-rightMax);
+				if(count>0) {
+					sum+=count;
+				}
+			}
+			System.out.println("#"+index+" "+sum);
+			index++;
+		}
+	}
+}
