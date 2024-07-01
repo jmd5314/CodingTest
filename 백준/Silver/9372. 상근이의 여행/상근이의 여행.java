1e5d6467de687d302afdf50d7c777cec07e5668d
jo.belief <jmd5314@gmail.com>
2024-07-01 19:46:05 +0900

    [Silver IV] Title: 상근이의 여행, Time: 308 ms, Memory: 44992 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-->0){
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            for(int i =0;i<M;i++){
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+            }
+            sb.append(N - 1).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
