0e0627c9a8508743b897d490ccae88479fbcd88c
조믿음 <jmd5314@gmail.com>
2025-05-03 14:20:43 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String,Integer> map = new HashMap<>();
+        for(String[] c:clothes){
+            map.put(c[1],map.getOrDefault(c[1],0)+1);
+        }
+        for(String s: map.keySet()){
+            answer*=map.get(s)+1;
+        }
+        return answer-1;
+    }
+}
