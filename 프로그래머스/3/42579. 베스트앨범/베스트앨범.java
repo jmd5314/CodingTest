343eddd1856c716d648b71f4a18cb43ed2c67243
조믿음 <jmd5314@gmail.com>
2026-05-07 19:19:39 +0900

    [level 3] Title: 베스트앨범, Time: 1.24 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,47 +1,38 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
+        HashMap<String, Integer> playCount = new HashMap<>();
+        HashMap<String,Map<Integer, Integer>> songInfo = new HashMap<>();
         List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> playCount = new HashMap<>();
-        Map<String, Map<Integer, Integer>> music = new HashMap<>();
-        for(int i =0;i < genres.length; i++){
-            String g = genres[i];
-            int p = plays[i];
-            // 장르를 처음 저장한다면
-            if(!playCount.containsKey(g)){
-                Map<Integer, Integer> map = new HashMap<>();
-                map.put(i,p);
-                playCount.put(g,p);
-                music.put(g,map);
+        
+        for(int i = 0; i < genres.length; i++){
+            playCount.put(genres[i],playCount.getOrDefault(genres[i],0)+plays[i]);
+            Map<Integer,Integer> map = songInfo.getOrDefault(genres[i],new HashMap<Integer,Integer>());
+            map.put(i,plays[i]);
+            songInfo.put(genres[i],map);
+            
             }
-            // 저장했던 장르라면
-            else{
-                playCount.put(g,playCount.get(g)+p);
-                Map<Integer, Integer> map = music.get(g);
-                map.put(i,p);
-                music.put(g,map);
-            }
-        }
-        List<String> keySet = new ArrayList<>(playCount.keySet());
-        Collections.sort(keySet,(o1,o2)->{
-          return playCount.get(o2) - playCount.get(o1);
-        });
-        for(String k : keySet){
-            Map<Integer, Integer> map = music.get(k);
-            List<Integer> musicNum = new ArrayList<>(map.keySet());
-            Collections.sort(musicNum,(o1,o2)->{
-               return map.get(o2) - map.get(o1);
+            
+            List<String> keySet = new ArrayList<>(playCount.keySet());
+            Collections.sort(keySet, (o1,o2) ->{
+                return playCount.get(o2) - playCount.get(o1);
             });
-            for(int i = 0;i<musicNum.size();i++){
-                answer.add(musicNum.get(i));
-                if(i == 1)
-                    break;
+            
+            for(String k : keySet){
+                Map<Integer, Integer> info = songInfo.get(k);
+                List<Integer> list = new ArrayList<>(info.keySet()); 
+                Collections.sort(list, (o1,o2)->{
+                    return info.get(o2) - info.get(o1);
+                });
+                answer.add(list.get(0));
+                if(list.size()>=2){
+                    answer.add(list.get(1));
+                }
             }
+        int [] arr = new int[answer.size()];
+        for(int i = 0; i<answer.size();i++){
+            arr[i] = answer.get(i);
         }
-        int [] result = new int [answer.size()];
-        for(int i = 0;i<result.length; i++){
-            result[i] = answer.get(i);
-        }
-        return result;
+        return arr;
     }
 }
