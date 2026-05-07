7a6174f972222636b3d3de4c023dcd7a77b91c9d
조믿음 <jmd5314@gmail.com>
2026-05-07 18:25:05 +0900

    [level 2] Title: 전화번호 목록, Time: 150.50 ms, Memory: 255 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,14 +1,20 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Arrays.sort(phone_book);
-        for(int i =0;i<phone_book.length-1;i++){
-            if(phone_book[i+1].startsWith(phone_book[i])){
-                answer =  false;
-                break;
+        HashMap<String, Boolean> map = new HashMap<>();
+        for(String num : phone_book){
+            map.put(num, true);
+        }
+        for(int i = 0; i < phone_book.length; i++){
+            String s = phone_book[i];
+            // 다 똑같은 경우가 존재하기 때문에 전체를 자르면 안됨
+            for(int j = 0; j < s.length() - 1; j++){
+                String prefix = s.substring(0,j + 1);
+                if(map.containsKey(prefix)){
+                    return false;
+                }
             }
         }
-        return answer;
+        return true;
     }
 }
